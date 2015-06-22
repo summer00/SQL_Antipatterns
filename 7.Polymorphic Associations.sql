3d8738d6cfa8daf584cef5fc928545e8eb21705d
summer00 <xiaright00@gmail.com>
2015-06-22 22:02:02 +0800

    第九章 元数据分裂

--- 7.Polymorphic Associations.sql
+++ 7.Polymorphic Associations.sql
@@ -1,6 +1,6 @@
 --描述：
 --		使用双用途的外键
---下面创建了一张评论表，该表通过issue_id关联Bugs或者FeatureRequests表，通过issue_type表面关联的是哪一张表。这种一个外键对应两张表的模式就是多态关联或者杂乱关联。
+-- 下面创建了一张评论表，该表通过issue_id关联Bugs或者FeatureRequests表，通过issue_type表面关联的是哪一张表。这种一个外键对应两张表的模式就是多态关联或者杂乱关联。
 CREATE TABLE Comments (
 	comment_id SERIAL PRIMARY KEY,
 	issue_type VARCHAR(20), -- "Bugs" or "FeatureRequests"
@@ -10,7 +10,7 @@ CREATE TABLE Comments (
 	comment TEXT,
 	FOREIGN KEY (author) REFERENCES Accounts(account_id)
 );
---创建另一张需使用的表
+-- 创建另一张需使用的表
 CREATE TABLE FeatureRequests (
 	issue_id SERIAL PRIMARY KEY,
 	reported_by BIGINT UNSIGNED NOT NULL,
@@ -24,9 +24,9 @@ CREATE TABLE FeatureRequests (
 	FOREIGN KEY (product_id) REFERENCES Product (product_id)
 )
 
---问题1：issue_id不重视一个外键，因而没有办法保证每一个issue_id都是真实存在的。同样也没法保证issue_type都是对应一张真实存在的表的。
+-- 问题1：issue_id不重视一个外键，因而没有办法保证每一个issue_id都是真实存在的。同样也没法保证issue_type都是对应一张真实存在的表的。
 
---问题2：使用多态关联进行查询
+-- 问题2：使用多态关联进行查询
 -- 要查找一条给定的评论对应的 Bug 记录或者特性需求，需要执行一条同时外联 Bugs 和FeatureRequests两张表的查询。但是仅有一张表的字段会显示出有值，我们需要的则是查询结果中的非空字段
 SELECT
 	*
@@ -36,14 +36,14 @@ LEFT JOIN bugs t2 ON t1.issue_id = t2.bug_id AND t1.issue_type = 'Bugs'
 LEFT JOIN featurerequests t3 ON t3.issue_id = t1.issue_id AND t1.issue_type = 'FeatureRequests'
 WHERE t1.comment_id = 2;
 
---合理使用反模式：
+-- 合理使用反模式：
 -- 你应该尽可能地避免使用多态关联——应该使用外键约束等来确保引用完整性。多态关联通常过度依赖上层程序代码而不是数据库的元数据。当你使用一个面向对象的框架（诸如 Hibernate）时，多态关联似乎是不可避免的。这种类型的框架通过良好的逻辑封装来减少使用多态关联的风险。
 
---解决：让关系变得简单
---解决1：反向引用
---当你看清楚问题的根源时，解决方案将变得异常的简单： 多态关联是一个反向关联。
+-- 解决：让关系变得简单
+-- 解决1：反向引用
+-- 当你看清楚问题的根源时，解决方案将变得异常的简单： 多态关联是一个反向关联。
 
---解决2：创建交叉表
+-- 解决2：创建交叉表
 -- Comments 表中的外键无法同时引用多张父表，因而，我们使用多个外键同时引用 Comments表即可。为每个父表创建一张独立的交叉表，每张交叉表同时包含一个指向Comments的外键和一个指向对应父表的外键。
 CREATE TABLE BugsComments (
 	i ssue_id BIGINT UNSIGNED NOT NULL,
@@ -62,9 +62,9 @@ CREATE TABLE FeaturesComments (
 	FOREIGN KEY (comment_id) REFERENCES Comments(comment_id)
 );
 -- 这个解决方案移除了对 Comments.issue_type 列的依赖。现在，元数据可以确保数据完整 性了，从而不再依赖于应用程序代码来维护数据间的关系。
---使用这个方法的时候，还需要注意的是，可以添加约束UNIQUE KEY (comment_id)来保证，一个评论只在交叉表中出现一次，但无法保证一个评论在多个交叉表中只出现一次，这部分功能需要通过程序来控制。
+-- 使用这个方法的时候，还需要注意的是，可以添加约束UNIQUE KEY (comment_id)来保证，一个评论只在交叉表中出现一次，但无法保证一个评论在多个交叉表中只出现一次，这部分功能需要通过程序来控制。
 
---解决3：使用连接查询
+-- 解决3：使用连接查询
 SELECT b.issue_id,
        b.description,
        b.reporter,
@@ -90,19 +90,19 @@ FROM Comments AS c
 JOIN (FeaturesComments
       JOIN FeatureRequests AS f USING (issue_id)) USING (comment_id)
 WHERE c.comment_id = 9876;
---另一种连接查询，使用COALESCE函数返回第一个非null的值
+-- 另一种连接查询，使用COALESCE函数返回第一个非null的值
 SELECT c. *,
-COALESCE(b.issue_id, f.issue_id ) AS issue_id,
-COALESCE(b.description, f.description) AS description,
-COALESCE(b.reporter, f.reporter ) AS reporter,
-COALESCE(b.priority, f.priority ) AS priority,
-COALESCE(b.status, f.status ) AS status,
-b.severity,
-b.version_affected,
-f.sponsor
+       COALESCE(b.issue_id, f.issue_id) AS issue_id,
+       COALESCE(b.description, f.description) AS description,
+       COALESCE(b.reporter, f.reporter) AS reporter,
+       COALESCE(b.priority, f.priority) AS priority,
+       COALESCE(b.status, f.status) AS status,
+       b.severity,
+       b.version_affected,
+       f.sponsor
 FROM Comments AS c
-LEFT OUTER JOIN (BugsComments JOIN Bugs AS b USING (issue_id))
-USING (comment_id)
-LEFT OUTER JOIN (FeaturesComments JOIN FeatureRequests AS f USING (issue_id))
-USING (comment_id)
+LEFT OUTER JOIN (BugsComments
+                 JOIN Bugs AS b USING (issue_id)) USING (comment_id)
+LEFT OUTER JOIN (FeaturesComments
+                 JOIN FeatureRequests AS f USING (issue_id)) USING (comment_id)
 WHERE c.comment_id = 9876;
--- 8.Multicolumn Attributes.sql
+++ 8.Multicolumn Attributes.sql
@@ -1,6 +1,6 @@
 --描述：
 --		存储多值属性
---一个属性（本例以给BUG添加标签为列）看上去属于一张表，但是存在多个值将多个值合并在一起并用逗号分隔导致难以对数据进行验证，难以读取或者改变单个值，同时也对聚合公式（诸如统计不同值的数量）非常不友好。学习过第二章，我们已经能够杜绝这种情况，所以本次要说明的反模式为使用多列存储这些值。
+-- 一个属性（本例以给BUG添加标签为列）看上去属于一张表，但是存在多个值将多个值合并在一起并用逗号分隔导致难以对数据进行验证，难以读取或者改变单个值，同时也对聚合公式（诸如统计不同值的数量）非常不友好。学习过第二章，我们已经能够杜绝这种情况，所以本次要说明的反模式为使用多列存储这些值。
 CREATE TABLE Bugs (
 	bug_id SERIAL PRIMARY KEY,
 	description VARCHAR(1000),
@@ -14,24 +14,24 @@ INSERT INTO `bugs_multicolumn_attributes` VALUES (3, '在采购草稿箱中的�
 INSERT INTO `bugs_multicolumn_attributes` VALUES (4, '订单提交失败', 'submit', 'error', NULL);
 INSERT INTO `bugs_multicolumn_attributes` VALUES (5, '无法选择产品', 'purchase', 'product', 'error');
 
---问题1：查询复杂
---当根据一个给定标签查询所有Bug记录时，你必须搜索所有的三列，因为这个标签字符串可能存放于这三列中的任何一列。
+-- 问题1：查询复杂
+-- 当根据一个给定标签查询所有Bug记录时，你必须搜索所有的三列，因为这个标签字符串可能存放于这三列中的任何一列。
 SELECT * FROM bugs_multicolumn_attributes
 WHERE tag1 = 'PURCHASE' OR tag2 = 'PURCHASE' OR tag3 ='PURCHASE';
---要查询标签为多个值时更加复杂
+-- 要查询标签为多个值时更加复杂
 SELECT * FROM bugs_multicolumn_attributes
 WHERE (tag1 = 'PURCHASE' OR tag2 = 'PURCHASE' OR tag3 ='PURCHASE')
 AND (tag1 = 'error' OR tag2 = 'error' OR tag3 ='error');
 
---问题2：添加及删除值
---当需要添加和删除一个标签的时候，这种方式也存在问题，我们必须先验证被更新的字段之前的值是否符合预期。
---删除work标签
+-- 问题2：添加及删除值
+-- 当需要添加和删除一个标签的时候，这种方式也存在问题，我们必须先验证被更新的字段之前的值是否符合预期。
+-- 删除work标签
 UPDATE Bugs
 SET tag1 = NULLIF(tag1, 'work'),
     tag2 = NULLIF(tag2, 'work'),
     tag3 = NULLIF(tag3, 'work')
 WHERE bug_id = 1;
---新增work标签
+-- 新增work标签
 -- 表达式将 performance 标签加到第一个空列中。然而，如果 3 列都不为空，这条语句将不对这条记录做任何修改，新的标签将不会被记录。同时，写这样的查询语句是非常繁琐耗时的，你必须要重复 performance 这个字符串 6 次！
 UPDATE bugs_multicolumn_attributes
    SET tag1 = CASE
@@ -45,21 +45,21 @@ UPDATE bugs_multicolumn_attributes
                 ELSE COALESCE(tag3, 'work') END
 WHERE bug_id = 1;
 
---问题3：无法确保唯一性
---你并不能阻止在一条记录中插入重复的标签
+-- 问题3：无法确保唯一性
+-- 你并不能阻止在一条记录中插入重复的标签
 INSERT INTO Bugs
   (description, tag1, tag2, tag3)
 VALUES
   ('printing is slow', 'printing', 'performance', 'performance');
 
---问题4：无法扩展
---如果我们想要加入标签大于预先设定的3个时，我们需要对表进行重构。重构的时候可能会导致锁住整张表，并阻止那些并发客户端的访问；加入新的列，必须去检查之前的代码是否会受到影响；有些数据库是通过定义一张符合需求的新表，然后将现有数据从旧表中复制到新表中，再丢弃旧表的方式来实现重构表结构的。如果需要重构的表有很多数据，那转换过程将·非常耗时。
+-- 问题4：无法扩展
+-- 如果我们想要加入标签大于预先设定的3个时，我们需要对表进行重构。重构的时候可能会导致锁住整张表，并阻止那些并发客户端的访问；加入新的列，必须去检查之前的代码是否会受到影响；有些数据库是通过定义一张符合需求的新表，然后将现有数据从旧表中复制到新表中，再丢弃旧表的方式来实现重构表结构的。如果需要重构的表有很多数据，那转换过程将·非常耗时。
 
---合理使用反模式：
---在某些情况下，一个属性可能有固定数量的候选值，并且对应的存储位置和顺序都是固定的。例如在Bugs表中定义三个不同的列来存储创建人、解决人、验收人这三个属性是合理的。
+-- 合理使用反模式：
+-- 在某些情况下，一个属性可能有固定数量的候选值，并且对应的存储位置和顺序都是固定的。例如在Bugs表中定义三个不同的列来存储创建人、解决人、验收人这三个属性是合理的。
 
---解决1：创建从属表
---如同在第二章中看到的一样，最好的解决方案是创建一张从属表，仅使用一列来存储多值属性，将多值属性存储在多行而不是多列中，在从表中添加外键，将这个值和Bugs表中的记录关联起来。
+-- 解决1：创建从属表
+-- 如同在第二章中看到的一样，最好的解决方案是创建一张从属表，仅使用一列来存储多值属性，将多值属性存储在多行而不是多列中，在从表中添加外键，将这个值和Bugs表中的记录关联起来。
 CREATE TABLE Tags_multicolumn_attributes (
 	bug_id BIGINT UNSIGNED NOT NULL,
 	tag VARCHAR(20),
@@ -72,13 +72,13 @@ VALUES
   (1, 'crash'),
   (2, 'printing'),
   (2, 'performance');
---在这种情况下，查找一个指定标签的bug就变得简单起来了。
+-- 在这种情况下，查找一个指定标签的bug就变得简单起来了。
 SELECT *
   FROM bugs_multicolumn_attributes T1
   JOIN Tags_multicolumn_attributes T2
     ON T1.bug_id = T2.BUG_ID
  WHERE T2.TAG = 'CRASH';
- --添加和删除标签也变得简单
+ -- 添加和删除标签也变得简单
 INSERT INTO Tags_multicolumn_attributes (bug_id, tag) VALUES (2, 'save');
 
 DELETE FROM Tags_multicolumn_attributes WHERE bug_id = 1 AND tag = 'crash';
