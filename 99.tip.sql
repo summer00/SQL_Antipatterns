f3f333da191239ecb036d41b32d1943383a86602
summer00 <xiaright00@gmail.com>
2015-06-25 11:26:00 +0800

    tip删除指定字段重复的数据

--- 99.tip.sql
+++ 99.tip.sql
@@ -1,11 +1,20 @@
-SQL复制表的几种方法：
+-- 1.SQL复制表的几种方法：
 
-SQL复制整张表
-
+-- SQL复制整张表:
 Select * Into New_Table_Name From Old_Table_Name;
-只复制列名而不要内容
-
+-- 只复制列名而不要内容:
 Select * Into New_Table_Name From Old_Table_Name Where 1 = 0;
-表间数据复制
-
+-- 表间数据复制:
 Insert Into New_Table_Name Select * From Old_Table_Name;
+
+-- 2.删除指定字段重复的数据：
+-- 添加ID
+ALTER TABLE T_TBM_CUSTOMER ADD Id int Identity(1,1);
+-- 删除重复，保存ID最小的
+DELETE
+FROM T_TBM_CUSTOMER
+WHERE ID NOT IN(
+	SELECT MIN(ID) FROM T_TBM_CUSTOMER
+    GROUP BY CUST_NAME, CUST_CONTACT_PHONE, SHOP_CODE, CREATED_BY, UPDATED_BY);
+-- 移除ID
+ALTER TABLE T_TBM_CUSTOMER DROP COLUMN id;
